f812f5742c387b56070590fa59084cbcd4d85108
Supakit <66056078@kmitl.ac.th>
2023-11-03 00:16:11 +0700

    Create app.py

---
+++ iris_data/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Iris")
+st.markdown('สร้าง `scatter plot` แสดงผลข้อมูล **Iris dataset**')
+
+choices = ['sepal.length',
+           'sepal.width',
+           'petal.length',
+           'petal.width']
+
+# https://docs.streamlit.io/library/api-reference/widgets/st.selectbox
+# 1. สร้าง st.selectbox ของ ตัวเลือก แกน x และ y จาก choices
+selected_x_var = st.selectbox('เลือกตัวแปรแกน x', choices)
+selected_y_var = st.selectbox('เลือกตัวแปรแกน y', choices)
+
+# https://docs.streamlit.io/library/api-reference/widgets/st.file_uploader
+# 2. สร้าง st.file_uploader เพื่อให้เลือกไฟล์ .csv เท่านั้น จากเครื่องผู้ใช้งาน
+iris_file = st.file_uploader('เพื่อให้เลือกไฟล์ iris.csv แล้ว Upload', type=['CSV'])
+
+if iris_file is not None:
+    iris_df = pd.read_csv(iris_file)
+else:
+    st.stop()
+
+st.subheader('ข้อมูลตัวอย่าง')
+# st.write(iris_df)
+
+st.subheader('แสดงผลข้อมูล')
+sns.set_style('darkgrid')
+markers = {"Setosa": "v", "Versicolor": "s", "Virginica": 'o'}
+
+fig, ax = plt.subplots()
+ax = sns.scatterplot(data=iris_df,
+                     x=selected_x_var, y=selected_y_var,
+                     hue='variety', markers=markers, style='variety')
+plt.xlabel(selected_x_var)
+plt.ylabel(selected_y_var)
+plt.title("Palmer's Penguins Data")
+st.pyplot(fig)
